dc07022ad9da6c0ec2686002cf99a10561d72fdb
Sneha Govindarajan <govindarajan.sn@northeastern.edu>
2022-12-03 19:43:11 -0500

    initial check

---
+++ src/main/java/com/ezkart/ezkart/EZKart.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.ezkart.ezkart;
+
+/**
+ *
+ * @author snehagovindarajan
+ */
+public class EZKart {
+
+    public static void main(String[] args) {
+        System.out.println("EzKart");
+    }
+}
